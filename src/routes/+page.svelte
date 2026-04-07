4abcc566eecd5d065e6e93c8ca3a64e513563bfa
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-04-07 17:40:45 +0530

    also some minor changes including some personal ranting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <main>
     <p style="font-size: 80px; font-style: normal;"><span><a class="titleSpan" target="_blank"
-                href="https://www.github.com/faqpy">faqpy</a></span> です!</p>
+                href="/about">faqpy</a></span> です!</p>
     <p style="font-size: 20px; font-style: normal;">(a.k.a witchie)</p>
     <p>Hello, I am Witchie, Thank you for visiting this page <CustomEmoji src={miauWhat} name=":miauWhat:"/> <CustomEmoji src={fw} name=":fw:"/></p>
     <a href="https://bsky.app/profile/faqpy.me" target="_blank"><button class="socialBtns">BSky</button></a>
