2630b1b770ecb3039b2826da318aa4cda7211f66
Witchie <73515621+Faqpy@users.noreply.github.com>
2026-04-08 11:22:06 +0530

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,7 @@
     <p style="font-size: 80px; font-style: normal;"><span><a class="titleSpan" target="_blank"
                 href="/">witchie</a></span> です!</p>
     <p>I commonly go by the name Witchie on most internet platforms and games. I don't have many friends because most people find me really unique, as I don't share similar interests as my friends, which makes me feel like I don't fit in anywhere, leading to feelings of disconnection and exclusion.<br></p>
-    <p>I also run a Hoshikawa Sara Fan Club! <CustomEmoji src={saraWink2} name=":SaraHappi:"/><CustomEmoji src={saraWink2} name=":SaraHappi:"/><CustomEmoji src={saraWink2} name=":SaraHappi:"/></p>
+    <p>I also run a Hoshikawa Sara Fan Club! <CustomEmoji src={saraHappi} name=":SaraHappi:"/><CustomEmoji src={saraHappi} name=":SaraHappi:"/><CustomEmoji src={saraHappi} name=":SaraHappi:"/></p>
     <p><span><a href="https://discord.gg/VQBfmCxeqZ" target="_blank" class="titleSpan" style="text-decoration: underline;">星川サラ•ファンクラブ</a></span></p>
 </main>
 <footer>
@@ -10,5 +10,5 @@
 </footer>
 <script>
     import CustomEmoji from "$lib/customEmoji.svelte";
-    const saraWink2 = "https://cdn.discordapp.com/emojis/1490319676931051571.webp?quality=lossless"
+    const saraHappi = "https://cdn.discordapp.com/emojis/1490319676931051571.webp?quality=lossless"
 </script>
