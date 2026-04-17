0440e7e67915f3d0282a6e1bb3e973ca0ed69d46
faqpy <73515621+Faqpy@users.noreply.github.com>
2026-04-18 00:22:08 +0530

    updated something

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,6 @@
 <main>
-    <p style="font-size: 80px; font-style: normal;"><span><a class="titleSpan" target="_blank"
-                href="/">witchie</a></span> です!</p>
-    <p>I commonly go by the name Witchie on most internet platforms and games. I don't have many friends because most people find me really unique, as I don't share similar interests as my friends, which makes me feel like I don't fit in anywhere, leading to feelings of disconnection and exclusion.<br></p>
+    <p style="font-size: 80px; font-style: normal;"><span><a class="titleSpan" href="/">witchie</a></span> です!</p>
+    <p>Heya, witchie here! I'm uhh a really mischievous person I would say.</p>
     <p>I also run a Hoshikawa Sara Fan Club! <CustomEmoji src={saraHappi} name=":SaraHappi:"/><CustomEmoji src={saraHappi} name=":SaraHappi:"/><CustomEmoji src={saraHappi} name=":SaraHappi:"/></p>
     <p><span><a href="https://discord.gg/VQBfmCxeqZ" target="_blank" class="titleSpan" style="text-decoration: underline;">星川サラ•ファンクラブ</a></span></p>
 </main>
